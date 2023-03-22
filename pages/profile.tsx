3de1008ddc42bd1badc4e346c310abf8c1aa8321
LeeChangHoon <changhoon1030@gmail.com>
2023-03-22 21:55:19 +0900

    🛠 fix : jpg => jpeg로 변경

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,7 +2,7 @@ import Layout from "../src/components/Common/Layout"
 import Head from "next/head"
 import Link from "next/link";
 import Image from "next/image";
-import profile from "../public/profile.jpg"
+import profile from "../public/profile.jpeg"
 const AboutMe = () => {
   return (
     <Layout>
