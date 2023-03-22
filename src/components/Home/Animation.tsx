2c0793937204220e0eafb335bd86fa6b1522d56b
LeeChangHoon <changhoon1030@gmail.com>
2023-03-22 18:31:17 +0900

    ⭐ feat : 이미지 관련 파일 업로드

---
+++ src/components/Home/Animation.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Lottie from "react-lottie-player"
+import lottieJson from "../../../public/dev.json"
+const Animation = () => {
+  return (
+    <Lottie
+      loop
+      animationData={lottieJson}
+      play
+    />
+  )
+}
+export default Animation
