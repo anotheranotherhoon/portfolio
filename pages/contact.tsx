264281c134a1ab5d5a6faca53e53a87b890ca685
LeeChangHoon <changhoon1030@gmail.com>
2023-03-23 01:04:03 +0900

    🎨 ui : footer 상단으로 올라오는 현상 해결

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -13,7 +13,7 @@ const contact = () => {
         <meta name="description" content="프론트엔드 개발자 이창훈 포트폴리오" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="text-gray-600 body-font h-full">
+      <section className="text-gray-600 body-font h-screen">
         <div className="container mx-auto flex px-5 py-24 justify-center flex-row gap-5">
           <div className="lg:w-1/5 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded">
             <Image
