e4e808f47dae43c6e343e108be68d243625b3f3a
LeeChangHoon <changhoon1030@gmail.com>
2023-03-22 18:40:04 +0900

    ⭐ feat : projects 컴포넌트

---
+++ pages/projects.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head"
+import Layout from "../src/components/Common/Layout"
+import axios from "axios";
+import { TOKEN, DATABASE_ID } from "../config";
+import ProjectItem from "../src/components/Project/ProjectItem";
+import type { ProjecInterface } from "../types/interface";
+
+
+interface ProjectInterface {
+  projects: ProjecInterface[];
+}
+
+const Projects = ({ projects }: ProjectInterface) => {
+  return (
+    <Layout>
+      <Head>
+        <title>프론트엔드 개발자 이창훈</title>
+        <meta name="description" content="프론트엔드 개발자 이창훈 포트폴리오" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col items-center justify-center min-h-screen mb-10 px-4">
+        <h1 className="text-4xl sm:text-6xl py-5 flex justify-center font-bold">프로젝트는 <span className="text-[#5367FF] ml-2">{projects.length}</span>개 입니다.</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 m-30 py-10 w-4/5">
+          {projects.map((project: ProjecInterface) => (
+            <ProjectItem key={project.id} projectInfo={project} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const getStaticProps = async () => {
+  const options = {
+    method: "POST",
+    url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`
+    },
+    data: { page_size: 100 }
+  };
+
+  const response = await axios.request(options)
+  const projects = await response.data.results
+  return {
+    props: {
+      projects
+    }
+  }
+}
+
+
+export default Projects
---
+++ src/components/Project/ProjectItem.tsx
@@ -0,0 +1,80 @@
+import type { ProjecInterface, TagType } from "../../../types/interface"
+import Image from "next/image";
+
+interface ProjectItemProps {
+  projectInfo: ProjecInterface;
+}
+
+const ProjectItem = ({ projectInfo }: ProjectItemProps) => {
+  const feature: string[] | undefined = projectInfo.properties.feature.rich_text[0]?.plain_text ? projectInfo.properties.feature.rich_text[0]?.plain_text.split(",") : undefined
+  const imgSrc = projectInfo.cover.file?.url || projectInfo.cover.external.url
+  const title = projectInfo.properties.Name.title[0].plain_text;
+  const description = projectInfo.properties.Description.rich_text[0].plain_text;
+  const deployUrl = projectInfo.properties.URL.url;
+  const githubUrl = projectInfo.properties.Github.url;
+  const portfolioUrl = projectInfo.properties.portfolio.url;
+  const coverUrl = projectInfo.properties.coverLink.url;
+  const start = projectInfo.properties.WorkPeriod.date.start;
+  const end = projectInfo.properties.WorkPeriod.date.end
+  const tags = projectInfo.properties.Tags.multi_select;
+  const projectType = projectInfo.properties.team.rich_text[0].plain_text;
+  const calculatedPeriod = (start: string, end: string) => {
+    const startDateStringArray = start.split("-");
+    const endDateStringArray = end.split("-");
+    var startDate = new Date(Number(startDateStringArray[0]), Number(startDateStringArray[1]), Number(startDateStringArray[2]));
+    var endDate = new Date(Number(endDateStringArray[0]), Number(endDateStringArray[1]), Number(endDateStringArray[2]));
+    const diffInMs = Math.abs(Number(endDate) - Number(startDate));
+    const result = diffInMs / (1000 * 60 * 60 * 24);
+    console.log(tags)
+    return result;
+  };
+  return (
+    <div className="project-card">
+      <a href={coverUrl}>
+        <Image
+          className="rounded-t-xl"
+          src={imgSrc}
+          width={100}
+          height={100}
+          alt="project cover image"
+          layout="responsive"
+          objectFit="none"
+          quality={100}
+        />
+      </a>
+      <div className="p-4 flex flex-col">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <h3 className="mt-4 text-lg">{description}</h3>
+        <h3 className="mt-3 mb-1">{projectType}</h3>
+        {feature ? <div className="mt-1 mb-4 p-1  bg-[#f3f4f6] rounded-md ">
+          <ul>
+            {feature.map((el: string, key: number) =>
+              <li key={key} className="text-slate-800 dark:text-slate-900">
+                {el}
+              </li>)}
+          </ul>
+        </div> : <></>
+        }
+        <a href={deployUrl}>🔗 배포 링크 바로가기</a>
+        <a href={githubUrl}>🔗 깃허브 링크 바로가기</a>
+        <a href={portfolioUrl}>🔗 회고 링크 바로가기</a>
+        <h4 className="my-2">
+          프로젝트 기간 : {start} ~ {end} (
+          {calculatedPeriod(start, end)}
+          일)
+        </h4>
+        <div className="flex items-start mt-2 overflow-auto ">
+          {tags!.map((aTag : TagType)   => (
+            <h1
+              className="px-1 py-1 mr-2 rounded-md bg-[#5367FF] dark:bg-[#5388FF] text-white dark:text-slate-900 w-30 "
+              key={aTag.id}
+            >
+              {aTag.name}
+            </h1>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ProjectItem
