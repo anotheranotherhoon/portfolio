dff35337549e85cb2d7f5dfac16ac785f0b3b8cb
LeeChangHoon <changhoon1030@gmail.com>
2023-03-24 02:34:51 +0900

    ⭐ feat : @svgr/webpack setting

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "www.notion.so", "s3.us-west-2.amazonaws.com", "drive.google.com"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
