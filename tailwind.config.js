2cf90e381fb4ef6dd47633d257bac1ff76a922af
LeeChangHoon <changhoon1030@gmail.com>
2023-03-28 15:40:01 +0900

    🧱 build : tailwindCSS 세팅 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
+  mode:"jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   darkMode : "class",
   theme: {
     extend: {
